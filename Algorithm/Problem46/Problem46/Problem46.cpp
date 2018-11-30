38fea76e51f45db6128d7fc4ecaab41a024808b5
Unknown <cielixer@gmail.com>
2018-11-30 23:30:15 +0900

    Problem46 - not all solved
    Solved except implementation of iterator

--- Algorithm/Problem46/Problem46/Problem46.cpp
+++ Algorithm/Problem46/Problem46/Problem46.cpp
@@ -1,21 +1,50 @@
-﻿// Problem46.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
+#include <vector>
+#include <array>
+
+template <typename T, int SIZE = 10>
+class CircularBuffer {
+private:
+	CircularBuffer() = default;
+
+public:
+	CircularBuffer(std::initializer_list<T> p_list)
+	{
+		static_assert(SIZE > 0);
+		static_assert(p_list.size() < SIZE);
+
+		std::copy(std::begin(p_list), std::end(p_list), std::begin(m_data));
+		m_movement = m_size = p_list.size();
+	}
+
+	inline auto empty() noexcept -> bool const { return m_size == 0; }
+	inline auto full() noexcept -> bool const { return m_size == SIZE; }
+	inline auto size() noexcept -> size_t const { return m_size; }
+	inline constexpr auto capacity() noexcept const { return SIZE; }
+
+	inline auto push(const T& value) noexcept -> void
+	{
+		m_data[(++m_movement) % SIZE] = value;
+		if (m_size < SIZE) ++m_size;
+	}
+
+	inline auto pop() noexcept -> void
+	{
+		if (m_size > 0) --m_size;
+		--m_movement;
+	}
+
+private:
+	std::array<T, SIZE> m_data;
+	size_t m_size = 0;
+
+	int m_movement = 0;
+
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
